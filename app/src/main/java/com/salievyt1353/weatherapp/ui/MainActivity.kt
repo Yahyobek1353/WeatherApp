8f59cf7972020155b2a724dc4e76546ef17bdb4d
KARIMZHANOV <karimzanovumar@gmail.com>
2024-03-20 15:14:56 +0300

    cik

--- app/src/main/java/com/salievyt1353/weatherapp/ui/MainActivity.kt
+++ app/src/main/java/com/salievyt1353/weatherapp/ui/MainActivity.kt
@@ -1,10 +1,11 @@
-package com.salievyt1353.weatherapp
+package com.salievyt1353.weatherapp.ui
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.salievyt1353.weatherapp.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
